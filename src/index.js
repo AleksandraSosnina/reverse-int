19c54781625d72c1325c6cc21b8c60ecf0b07cce
Sosnina Sasha <aleksandra.sosnina@unitbean.com>
2020-10-15 16:32:58 +0400

    reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+function reverse(num) {
+    const isNegative = num[0] == '-';
+
+    if (isNegative) {
+        num = num.str.slice(1);
+    }
+
+    return (
+        parseFloat(
+            num
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        )
+    );
 }
+
+module.exports = reverse;
